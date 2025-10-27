c60f85fdeda502ca9b882fa27c9fed588e845fe1
Chrissie22 <christabel.ojobolo@gmail.com>
2025-10-27 22:49:26 +0100

    Register Task model in Django admin panel

--- todo/admin.py
+++ todo/admin.py
@@ -5,5 +5,6 @@ from . models import Task
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('task', 'is_completed', 'updated_at')
     search_fields = ('task',)
+    
 
 admin.site.register(Task, TaskAdmin)
