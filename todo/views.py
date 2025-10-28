d37c30b72ea03f68359512defc79fbe5292a5af6
Chrissie22 <christabel.ojobolo@gmail.com>
2025-10-28 12:30:36 +0100

    Feat: implement 'Mark as Undone' functionality for completed tasks

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
-from . models import Task
+from .models import Task
 
 # Create your views here.
 def addTask(request):
@@ -11,5 +11,11 @@ def addTask(request):
 def mark_as_done(request, pk):
     task = get_object_or_404(Task, pk=pk)
     task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
     task.save()
     return redirect('home')
